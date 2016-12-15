8d4c3f21a118c6b55e58bff27f465acc33ca5004
elgrillo83 <elgrillo83@gmail.com>
2016-12-15 13:33:45 +0100

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install macOS applications
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install spectacle
