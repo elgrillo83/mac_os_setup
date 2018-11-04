2bcf960ea18044bd2ec94916dfaa0814cc0bdc6d
Thilo Grashei <elgrillo83@gmail.com>
2018-11-04 20:36:10 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,24 +4,28 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install macOS applications
-brew cask install google-chrome
 brew cask install dropbox
+brew cask install gitkraken
+brew cask install google-chrome
 brew cask install iterm2
 brew cask install macvim
-brew cask install robomongo
-brew cask install spectacle
-brew cask install radiant-player
-brew cask install flash-npapi # needed for radiant-player
+brew cask install postico
 brew cask install sequel-pro
+brew cask install spectacle
 
 # Install rbenv and some ~ruby versions
 brew install rbenv
-rbenv install 2.2.4
+rbenv init
 rbenv install 2.3.1
+rbenv install 2.5.3
 
 # Install mysql (mariadb)
 brew install mariadb
 brew services start mariadb
+
+# Install postgresql
+brew install postgresql
+brew services start postgresql
 
 # Install redis
 brew install redis
